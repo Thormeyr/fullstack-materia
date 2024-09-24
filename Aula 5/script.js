6eb7aef571d4425decb75cb4713daa272ac51af0
Pedro Thormeyr <84100284+Thormeyr@users.noreply.github.com>
2024-09-23 22:27:56 -0300

    aula 5

---
+++ Aula 5/script.js
@@ -0,0 +1,82 @@
+// let entrada = prompt("Falaa ai");
+// window.alert("ce falou isso cara:"+entrada);
+// console.log("falando escondido agr se liga:"+entrada)
+
+for(let i=0;i<100;i++){
+    if((i%2)!=0){
+        console.log(i+"\n");
+
+    }
+}
+
+console.log("O outro agr")
+
+for(let i=0;i<500;i++){
+    if((i%5)==0){
+        console.log(i+"\n");
+
+    }
+}
+
+
+let numero = prompt("bota o numero ai: ")
+
+while(numero>=0){
+    console.log(numero+"\n");
+    numero--;
+}
+
+let numero2 = prompt("faz fatorial desse trem:")
+let aux = 1
+while(numero2>0){
+    
+    aux = aux*numero2;
+    numero2--;
+
+}
+console.log("aaa:"+aux)
+
+let totalSalario = 0;
+let quantidadePessoas = 0;
+let maiorIdade = -Infinity;
+let menorIdade = Infinity;
+let mulheresComSalarioAte5000 = 0;
+
+while (true) {
+    let idade = parseInt(prompt("Digite a idade (ou uma idade negativa para encerrar):"));
+
+    if (idade < 0) {
+        break; // Encerra o loop quando a idade negativa é digitada
+    }
+
+    let sexo = prompt("Digite o sexo (M/F):").toUpperCase();
+    let salario = parseFloat(prompt("Digite o salário:"));
+
+    // Somar o salário ao total e contar a pessoa
+    totalSalario += salario;
+    quantidadePessoas++;
+
+    // Atualizar maior e menor idade
+    if (idade > maiorIdade) {
+        maiorIdade = idade;
+    }
+    if (idade < menorIdade) {
+        menorIdade = idade;
+    }
+
+    // Contar mulheres com salário até 5000
+    if (sexo === 'F' && salario <= 5000) {
+        mulheresComSalarioAte5000++;
+    }
+}
+
+if (quantidadePessoas > 0) {
+    let mediaSalario = totalSalario / quantidadePessoas;
+
+    console.log("Média de salário do grupo: R$", mediaSalario.toFixed(2));
+    console.log("Maior idade do grupo:", maiorIdade);
+    console.log("Menor idade do grupo:", menorIdade);
+    console.log("Quantidade de mulheres com salário até R$5000,00:", mulheresComSalarioAte5000);
+} else {
+    console.log("Nenhuma pessoa foi cadastrada.");
+}
