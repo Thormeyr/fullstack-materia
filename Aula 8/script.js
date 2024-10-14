58eecc8e710e5a9801ebd2cdcd51650f88e4b541
Pedro Thormeyr <84100284+Thormeyr@users.noreply.github.com>
2024-10-14 20:15:14 -0300

    adding animation

--- Aula 8/script.js
+++ Aula 8/script.js
@@ -36,8 +36,32 @@ let retangulo2 = {
     }
 }
 
+function animacao(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    
+    
+    retangulo.desenha();
+    retangulo2.desenha();
+    requestAnimationFrame(animacao);
+}
+
 retangulo.desenha();
 retangulo2.desenha();
+
+animacao();
+
+document.addEventListener("keydown", function(event){
+    if(event.key == "ArrowLeft"){
+        retangulo.x -= 5;
+    }else if(event.key == "ArrowRight"){
+        retangulo.x += 5;
+    }else if(event.key == "ArrowUp"){
+        retangulo.y -= 5;
+    }else if(event.key == "ArrowDown"){
+        retangulo.y += 5;
+    }
+});
+
 
 
 
